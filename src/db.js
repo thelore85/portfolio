d41060b83e497db02029214a8662f27bc33fa7af
TheLore85 <garofalo.digital@gmail.com>
2023-08-30 20:41:49 +0200

    db: project update v3

--- src/db.js
+++ src/db.js
@@ -8,7 +8,7 @@ export const pjt =[
     title: "Vocabulary Enhancer",
     description: "An innovative way of learning a foraing language using the 'Vocabolary List Method'. Pick your level and exercice the list for the week, you'll master your new vocabolary i a while!",
     tech: "A react based project structured on a folder jerarquy able to scale in fucntionality and levels. It use API inheritance for words lists. This is an open source project where anyone can give its support and inhance the app",
-    tag: [ 'react', 'api', 'css', ],
+    tag: [ 'react', 'api', 'css', 'app'],
     color: [ 123, 231, 56],
     gitCode: 'https://github.com/thelore85/project-vocabulary',
     gitPage: 'https://thelore85.github.io/project-vocabulary/'
@@ -17,13 +17,13 @@ export const pjt =[
 
     { 
     id: 2,
-    title: "Background Generator",
+    title: "Face Recognition App",
     description: 'A super handy program that makes css gradient background css easy and funny. Just pick the color from the input and copy the css code generated!',
     tech: 'A pure vanilla Javascript project. Single Html page with a classic .css and .js files conneted',
-    tag: [ 'js', 'css'],
+    tag: [ 'js', 'app', 'machine-learning', 'api', 'css', 'react', 'sql'],
     color: [ 132, 160, 6 ],
-    gitCode: 'https://github.com',
-    gitPage: 'https://www.piqus.it'
+    gitCode: 'https://github.com/thelore85/project-face-recognition',
+    gitPage: 'https://component-login.vercel.app/'
     
     },
 
@@ -32,7 +32,7 @@ export const pjt =[
     title: "Picture-in-Picture Web App",
     description: "This project is a JavaScript application that utilizes the browser's mediaDevices web API to offer Picture-in-Picture (PiP) functionality. With this app, you can select a screen area and display it in a separate PiP window.",
     tech: 'Pure vanilla Javasctip and html css files. Managing complex data structure such as objects and array with loops and functions',
-    tag: [ 'js', 'css', 'api' ],
+    tag: [ 'js', 'css', 'api', 'app' ],
     color: [ 236, 23, 154],
     gitCode: 'https://github.com/thelore85/picture-in-picture',
     gitPage: 'https://thelore85.github.io/picture-in-picture/'
@@ -45,7 +45,7 @@ export const pjt =[
     description: "An innovative way of leartning a foraing language using the 'Vocabolary List Method'. Pisk your level and exercice the list for the week, you'll master your new vocabolary i a while!",
     tech: 'Pure vanilla Javasctip and html css files. This project use font-face and API import to gather third party data in the app',
     tag: [ 'js', 'api', 'css'],
-    color: [ 12, 215, 245],
+    color: [ 12, 215, 24],
     gitCode: 'https://github.com/thelore85/project-quote-generator',
     gitPage: 'https://thelore85.github.io/project-quote-generator/'
     
@@ -56,11 +56,23 @@ export const pjt =[
       title: "Roialty Free Image - Search Engine",
       description: "A React project for a royalty free images search engine. The project sources images through APIs and uses JavaScript to achieve an infinite scrolling effect.",
       tech: '',
-      tag: [ 'React', 'api', 'css' ],
-      color: [ 123, 151, 25],
+      tag: [ 'React', 'api', 'css', 'app' ],
+      color: [ 13, 151, 25],
       gitCode: 'https://github.com/thelore85/project-search-engine',
-      gitPage: 'https://vercel.com/thelore85/project-search-engine'
+      gitPage: 'https://project-search-engine-six.vercel.app/'
       
-      },
+    },
+
+    { 
+      id: 6,
+      title: "Login component",
+      description: "A React project for a royalty free images search engine. The project sources images through APIs and uses JavaScript to achieve an infinite scrolling effect.",
+      tech: '',
+      tag: [ 'React', 'component'],
+      color: [ 137, 175, 205],
+      gitCode: 'https://github.com/thelore85/component-login',
+      gitPage: 'https://component-login.vercel.app/'
+      
+    },
 
 ]
