dfda06ac94ca9eeb920f88d4599a03877284ba5e
TheLore85 <garofalo.digital@gmail.com>
2023-09-02 18:45:18 +0200

    db update

--- public/db.js
+++ public/db.js
@@ -7,8 +7,7 @@ export const pjt =[
     id: 1,
     title: "Vocabulary Enhancer",
     description: "An innovative way of learning a foraing language using the 'Vocabolary List Method'. Pick your level and exercice the list for the week, you'll master your new vocabolary i a while!",
-    tech: "A react based project structured on a folder jerarquy able to scale in fucntionality and levels. It use API inheritance for words lists. This is an open source project where anyone can give its support and inhance the app",
-    tag: [ 'react', 'api', 'css', 'app'],
+    tag: [ 'web-app','react', 'api', 'app'],
     color: [ 123, 231, 56],
     gitCode: 'https://github.com/thelore85/project-vocabulary',
     gitPage: 'https://thelore85.github.io/project-vocabulary/'
@@ -17,10 +16,9 @@ export const pjt =[
 
     { 
     id: 2,
-    title: "Face Recognition App",
+    title: "Face Recognition",
     description: 'A super handy program that makes css gradient background css easy and funny. Just pick the color from the input and copy the css code generated!',
-    tech: 'A pure vanilla Javascript project. Single Html page with a classic .css and .js files conneted',
-    tag: [ 'js', 'app', 'machine-learning', 'api', 'css', 'react', 'sql'],
+    tag: [ 'web-app', 'js', 'app', 'machine-learning', 'api', 'react', 'sql'],
     color: [ 132, 160, 6 ],
     gitCode: 'https://github.com/thelore85/project-face-recognition',
     gitPage: 'https://component-login.vercel.app/'
@@ -29,10 +27,9 @@ export const pjt =[
 
     { 
     id: 3,
-    title: "Picture-in-Picture Web App",
+    title: "Picture-in-Picture",
     description: "This project is a JavaScript application that utilizes the browser's mediaDevices web API to offer Picture-in-Picture (PiP) functionality. With this app, you can select a screen area and display it in a separate PiP window.",
-    tech: 'Pure vanilla Javasctip and html css files. Managing complex data structure such as objects and array with loops and functions',
-    tag: [ 'js', 'css', 'api', 'app' ],
+    tag: [ 'web-app', 'api', 'vanilla' ],
     color: [ 236, 23, 154],
     gitCode: 'https://github.com/thelore85/picture-in-picture',
     gitPage: 'https://thelore85.github.io/picture-in-picture/'
@@ -43,8 +40,7 @@ export const pjt =[
     id: 4,
     title: "Quote Generator",
     description: "An innovative way of leartning a foraing language using the 'Vocabolary List Method'. Pisk your level and exercice the list for the week, you'll master your new vocabolary i a while!",
-    tech: 'Pure vanilla Javasctip and html css files. This project use font-face and API import to gather third party data in the app',
-    tag: [ 'js', 'api', 'css'],
+    tag: ['css', 'vanilla'],
     color: [ 12, 215, 24],
     gitCode: 'https://github.com/thelore85/project-quote-generator',
     gitPage: 'https://thelore85.github.io/project-quote-generator/'
@@ -53,10 +49,10 @@ export const pjt =[
 
     { 
       id: 5,
-      title: "Roialty Free Image - Search Engine",
+      title: "Images Search Engine",
       description: "A React project for a royalty free images search engine. The project sources images through APIs and uses JavaScript to achieve an infinite scrolling effect.",
       tech: '',
-      tag: [ 'react', 'api', 'css', 'app' ],
+      tag: [ 'web-app', 'react', 'api', 'css'],
       color: [ 13, 151, 25],
       gitCode: 'https://github.com/thelore85/project-search-engine',
       gitPage: 'https://project-search-engine-six.vercel.app/'
--- src/components/Projects/Selector/Selector.js
+++ src/components/Projects/Selector/Selector.js
@@ -16,13 +16,13 @@ const Selector = ({ onProjcetSelection }) =>{
     <div className="selector-wrapper">
       <select className="selector" onChange={onProjcetSelection}>
         <option className= "level" value="" defaultValue>All Projects</option>
-        <option className= "level" value="react">React.js</option>
-        <option className= "level" value="vanilla">Vanilla</option>
+        <option className= "level" value="web-app">Web Application</option>
         <option className= "level" value="machine-learning"> AI / Machine Learning</option>
         <option className= "level" value="api">API integration</option>
-        <option className= "level" value="sql">Full-Stuck</option>
+        <option className= "level" value="sql">Back-end</option>
+        <option className= "level" value="css">CSS & Typografy</option>
         <option className= "level" value="component">Components</option>
-        <option className= "level" value="template">Template</option>
+        <option className= "level" value="template">Templates</option>
       </select>
     </div>
     </div>
