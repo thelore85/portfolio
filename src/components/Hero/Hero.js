9078a380c78e20707f026bdb874284ab639fe3fb
TheLore85 <garofalo.digital@gmail.com>
2023-09-15 22:12:54 +0200

    claim update

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className="wrapper row vertical-align">
       
         <div className="hero-content">
-          <h1>Keep calm and <span>develop</span> a website</h1>
+          <h1>Keep calm and <span>develop</span> a web app</h1>
           <a type="button" className="btn btn-secondary" href="#project">view portfolio</a>
           <a type="button" className="btn btn-primary" href="#about">contact me</a>
         </div>
