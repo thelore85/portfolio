2ec25d25dd224a519ea2b106f90604f351eb573e
TheLore85 <garofalo.digital@gmail.com>
2023-09-28 14:34:18 +0200

    db: get rid of production link for Marriage ste project

--- public/db.js
+++ public/db.js
@@ -101,7 +101,7 @@ export const pjt =[
 
     { 
       id: 9,
-      title: "React Template - Vanilla",
+      title: "React Only Template",
       description: "Starter template for building React applications. It provides a structured foundation for creating web applications using React.js.",
       tech: ['react', 'webpack', 'node.js'],
       tag: [ 'template'],
@@ -138,12 +138,12 @@ export const pjt =[
       tech: ['react', 'next.js', 'node.js'],
       tag: [ 'project'],
       color: [ 107, 190, 230],
-      gitCode: 'https://github.com/thelore85/project-marriage-ste',
-      gitPage: 'https://project-marriage-ste.vercel.app/',
+      gitCode: 'https://github.com/thelore85/component-landing-next',
+      gitPage: 'https://component-landing-next.vercel.app/',
     },
     { 
       id: 13,
-      title: "Landing Page Component - Next.js",
+      title: "Landing Page - Next.js",
       description: "Landing Page Component developed using Next.js and React, it offers modularity, performance optimization, and mobile responsiveness, ensuring a seamless user experience.",
       tech: ['react', 'next.js', 'node.js'],
       tag: [ 'component'],
